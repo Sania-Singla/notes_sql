00ae412c5b1f568f2d9fd8aee524e24fcf9ee6e8
Sania Singla <singlasania7717@gmail.com>
2024-08-27 01:23:06 +0530

    added database.js

--- backend/database.js
+++ backend/database.js
@@ -2,6 +2,8 @@ import mysql from "mysql2";
 
 export let pool;
 
+//⭐⭐so instead of connecting to the db everytime when querying we create a pool of all the connections similar to what we did in the db/connection file in tweettube (we got a object where .connection was a property)
+
 export async function connect_db () {  // function is returning a response so should be an async function
     pool = mysql.createPool({
         host: process.env.MYSQL_HOST,
