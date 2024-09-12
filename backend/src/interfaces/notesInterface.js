fe9116adcfebe2d4308c5a2098158aef96505587
Sania Singla <singlasania7717@gmail.com>
2024-09-12 15:06:36 +0530

    correction

--- backend/src/interfaces/notesInterface.js
+++ backend/src/interfaces/notesInterface.js
@@ -1,25 +1,25 @@
 export class Inotes {
     async getNotes() {
-        throw new Error("method get_all_notes is not overwritten.");
+        throw new Error("method get_notes is not overwritten.")
     }
 
     async getNote(id) {
-        throw new Error("method get_a_note is not overwritten.");
+        throw new Error("method get_note is not overwritten.")
     }
 
     async deleteNotes() {
-        throw new Error("method delete_all_notes is not overwritten.");
+        throw new Error("method delete_notes is not overwritten.")
     }
 
     async deleteNote(id) {
-        throw new Error("method delete_a_note is not overwritten.");
+        throw new Error("method delete_note is not overwritten.")
     }
 
     async createNote(id, title, content) {
-        throw new Error("method create_a_note is not overwritten.");
+        throw new Error("method create_note is not overwritten.")
     }
 
     async editNote(id, title, content) {
-        throw new Error("method edit_a_note is not overwritten.");
+        throw new Error("method edit_note is not overwritten.")
     }
 }
